8b564ae1d10ebb7ccc6cc92cf1f20671a2ff2e9d
chenpengcode <cpcoder@aliyun.com>
2020-12-21 22:17:56 +0800

    动态规划 背包问题

--- src/main/java/algorithm/dp/Pack.java
+++ src/main/java/algorithm/dp/Pack.java
@@ -8,25 +8,30 @@ package algorithm.dp;
 public class Pack {
     public int zeroOnePack(int[] w, int[] v, int capacity) {
         int n = w.length;
-        int[][] dp = new int[n + 1][capacity + 1];
-        // dp[0][0…W] = 0, 前0个物品（即没有物品）装入书包的最大价值为0
-        // i 从1开始
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= capacity; j++) {
-                if (j < w[i - 1]) dp[i][j] = dp[i - 1][j];
-                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + v[i - 1]);
+        int[][] dp = new int[n][capacity + 1];
+        for (int j = 0; j <= capacity; j++) {
+            if (j < w[0]) dp[0][j] = 0;
+            else dp[0][j] = v[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = w[i]; j <= capacity; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
             }
         }
-        return dp[n][capacity];
+        return dp[n - 1][capacity];
     }
 
     public int zeroOnePack2(int[] w, int[] v, int capacity) {
         int n = w.length;
         int[] dp = new int[capacity + 1];
+        for (int j = 0; j <= capacity; j++) {
+            if (j >= w[0]) dp[j] = v[0];
+            else dp[j] = 0;
+        }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = capacity; j >= w[i - 1]; --j) {
-                dp[j] = Math.max(dp[j - w[i - 1]] + v[i - 1], dp[j]);
+        for (int i = 1; i < n; i++) {
+            for (int j = capacity; j >= w[i]; --j) {
+                dp[j] = Math.max(dp[j - w[i]] + v[i], dp[j]);
             }
         }
         return dp[capacity];
@@ -34,48 +39,66 @@ public class Pack {
 
     public int unboundedPack(int[] w, int[] v, int capacity) {
         int n = w.length;
-        int[][] dp = new int[n + 1][capacity + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = w[i - 1]; j <= capacity; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - w[i - 1]] + v[i - 1]);
+        int[][] dp = new int[n][capacity + 1];
+        for (int j = 0; j <= capacity; ++j) {
+            if (j >= w[0]) dp[0][j] = (j / w[0]) * v[0];
+            else dp[0][j] = 0;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = w[i]; j <= capacity; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - w[i]] + v[i]);
             }
         }
-        return dp[n][capacity];
+        return dp[n - 1][capacity];
     }
 
     public int unboundedPack2(int[] w, int[] v, int capacity) {
         int n = w.length;
         int[] dp = new int[capacity + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = w[i - 1]; j <= capacity; j++) {
-                dp[j] = Math.max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
+        for (int j = 0; j <= capacity; ++j) {
+            if (j >= w[0]) dp[j] = (j / w[0]) * v[0];
+            else dp[j] = 0;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = w[i]; j <= capacity; j++) {
+                dp[j] = Math.max(dp[j], dp[j - w[i]] + v[i]);
             }
         }
         return dp[capacity];
     }
 
     public int unboundedPack3(int[] w, int[] v, int capacity) {
-        int n = w.length;
-        int[][] dp = new int[n + 1][capacity + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = w[i - 1]; j <= capacity; j++) {
-                int cnt = j / w[i - 1];
+        int n = v.length;
+        int[][] dp = new int[n][capacity + 1];
+
+        for (int j = 0; j <= capacity; ++j) {
+            if (j >= w[0]) dp[0][j] = (j / w[0]) * v[0];
+            else dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = w[i]; j <= capacity; j++) {
+                int cnt = j / w[i];
                 for (int k = 0; k <= cnt; k++) {
-                    dp[i][j] = Math.max(dp[i - 1][j - k * w[i - 1]] + k * v[i - 1], dp[i][j]);
+                    dp[i][j] = Math.max(dp[i - 1][j - k * w[i]] + k * v[i], dp[i][j]);
                 }
             }
         }
-        return dp[n][capacity];
+        return dp[n - 1][capacity];
     }
 
     public int unboundedPack4(int[] w, int[] v, int capacity) {
         int n = w.length;
         int[] dp = new int[capacity + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = w[i - 1]; j <= capacity; j++) {
-                int cnt = j / w[i - 1];
-                for (int k = 0; k <= cnt; k++) {
-                    dp[j] = Math.max(dp[j - k * w[i - 1]] + k * v[i - 1], dp[j]);
+        for (int j = 0; j <= capacity; ++j) {
+            if (j >= w[0]) dp[j] = (j / w[0]) * v[0];
+            else dp[j] = 0;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = capacity; j >= w[i]; j--) {
+                int cnt = j / w[i];
+                for (int k = 1; k <= cnt; k++) {
+                    dp[j] = Math.max(dp[j - k * w[i]] + k * v[i], dp[j]);
                 }
             }
         }
@@ -84,26 +107,34 @@ public class Pack {
 
     public int boundedPack(int[] w, int[] v, int[] count, int capacity) {
         int n = count.length;
-        int[][] dp = new int[n + 1][capacity + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = w[i - 1]; j <= capacity; j++) {
-                int cnt = Math.min(j / w[i - 1], count[i - 1]);
-                for (int k = 0; k <= cnt; k++) {
-                    dp[i][j] = Math.max(dp[i - 1][j - k * w[i - 1]] + k * v[i - 1], dp[i][j]);
+        int[][] dp = new int[n][capacity + 1];
+        for (int j = 0; j <= capacity; ++j) {
+            if (j >= w[0]) dp[0][j] = (Math.min(j / w[0], count[0])) * v[0];
+            else dp[0][j] = 0;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = w[i]; j <= capacity; j++) {
+                int cnt = Math.min(j / w[i], count[i]);
+                for (int k = 1; k <= cnt; k++) {
+                    dp[i][j] = Math.max(dp[i - 1][j - k * w[i]] + k * v[i], dp[i][j]);
                 }
             }
         }
-        return dp[n][capacity];
+        return dp[n - 1][capacity];
     }
 
     public int boundedPack2(int[] w, int[] v, int[] count, int capacity) {
         int n = w.length;
         int[] dp = new int[capacity + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = capacity; j >= w[i - 1]; j--) {
+        for (int j = 0; j <= capacity; ++j) {
+            if (j >= w[0]) dp[j] = (Math.min(j / w[0], count[0])) * v[0];
+            else dp[j] = 0;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = capacity; j >= w[i]; j--) {
                 int cnt = Math.min(j / w[i - 1], count[i - 1]);
                 for (int k = 0; k <= cnt; k++) {
-                    dp[j] = Math.max(dp[j - k * w[i - 1]] + k * v[i - 1], dp[j]);
+                    dp[j] = Math.max(dp[j - k * w[i]] + k * v[i], dp[j]);
                 }
             }
         }
@@ -111,12 +142,12 @@ public class Pack {
     }
 
     public static void main(String[] args) {
-        int[] w = {4, 3, 2};
-        int[] v = {2, 5, 1};
-        int[] n = {3, 2, 5};
+        int[] w = {4, 3, 2, 2};
+        int[] v = {2, 4, 1, 3};
+        int[] n = {3, 2, 5, 4};
         int capacity = 6;
         Pack pack = new Pack();
-        System.out.println(pack.unboundedPack4(w, v, capacity));
+//        System.out.println(pack.unboundedPack4(w, v, capacity));
         System.out.println(pack.boundedPack2(w, v, n, capacity));
     }
 }
